afeda1ebd2d6659ae349f5438b92ba517708acd4
sugayoong <151974452+sugayoong@users.noreply.github.com>
2024-08-23 04:29:13 +0530

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,16 @@
+function updateTime() {
+    const timeElement = document.getElementById('time');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    timeElement.textContent = currentTime;
+}
+
+
+setInterval(updateTime, 1000);
+
+
+updateTime();
